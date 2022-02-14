df834d94e4229deb3955ef61050d60189debe29a
MostafaFouad <tee.fouad@gmail.com>
2022-02-14 14:27:38 +0200

    Update type definitions

--- index.d.ts
+++ index.d.ts
@@ -1,39 +1,21 @@
 import { Store, Action, Middleware } from 'redux';
 
 declare module 'speedux' {
-  declare type GlobalStateObject = {
+  export type GlobalStateUpdate<T> = Partial<T> | ((prevState: T) => Partial<T>);
+
+  export type GlobalStateObject = {
     [path: string]: any;
   };
 
-  declare type GlobalStateActions<T> = {
-    [actionName: string]: (...args) => Partial<T>;
-  };
-
-  declare type GlobalStateHandlers<T> = {
-    [actionType: string]: (action: Action) => Partial<T>;
-  };
-
-  declare type ActionDispatchers = {
-    [actionName: string]: (...args) => void;
-  };
-
-  declare type DispatchFunction = {
-    (action: string | Action): void;
-  };
-
-  declare type HandlerCallback = {
-    (action: Action): void;
-  };
-
-  declare type ReducerFunction<T> = {
-    (state: T, action: Action): T;
+  export type GlobalStateActions<T> = {
+    [actionName: string]: (...args) => GlobalStateUpdate<T>;
   };
 
   /**
    * An object that describes the global state and the actions and handlers
    * associated to it.
    */
-  declare type GlobalStateConfig<T extends GlobalStateObject, U extends GlobalStateActions<T>> = {
+  export type GlobalStateConfig<T extends GlobalStateObject, U extends GlobalStateActions<T>> = {
     /**
      * Key that should be used to create this global state in the Redux store.
      */
@@ -74,54 +56,77 @@ declare module 'speedux' {
      * }
      * ```
      */
-    handlers?: U;
+    handlers?: {
+      [actionType: string]: (action: Action) => GlobalStateUpdate<T>;
+    };
+  };
+
+  export type DispatchFunction = {
+    (action: string | Action): void;
+  };
+
+  export type HandlerCallback = {
+    (action: Action): void;
+  };
+
+  export type Yieldable<T> = T | Promise<unknown> | unknown;
+
+  export type ExecuteFunction<T> = (...args) => Promise<T>;
+
+  export type CancelFunction = () => void;
+
+  export type ReducerFunction<T> = {
+    (state: T, action: Action): T;
   };
 
   /**
    * Creates a new global state branch in the Redux store.
    * @param config An object that describes the global state and the actions and handlers associated to it.
    */
-  declare function createGlobalState<T, U>(config: GlobalStateConfig<T, U>): void;
+  export function createGlobalState<T, U>(config: GlobalStateConfig<T, U>): void;
 
   /**
    * Returns a piece of global state from the store. This is a hook function and can only be used inside a component.
    * @param query Name of global state to read or a query string.
    * @returns The global state object that corresponds to the query.
    */
-  declare function useGlobalState(query?: string): any;
+  export function useGlobalState<T>(query?: string): T;
 
   /**
    * Returns a piece of global state from the store based on the provided query.
    * @param query Query string.
    * @returns The global state object that corresponds to the query.
    */
-  declare function queryGlobalState(query?: string): any;
+  export function queryGlobalState<T>(query?: string): T;
 
   /**
    * Returns a list of dispatcher functions that correspond to the configured `actions` field.
    * @param name Name of the global state to which the actions are associated.
    */
-  declare function useActions<T extends ActionDispatchers>(name: string): T;
+  export function useActions<T>(name: string): T;
 
   /**
    * Returns a `dispatch` function which can be used to dispatch actions.
    * @param name Name of the global state to which the dispatched actions should be associated.
    */
-  declare function useDispatch(name?: string): DispatchFunction;
+  export function useDispatch(name?: string): DispatchFunction;
 
   /**
    * Allows listening to any action dispatched by the Redux store.
    * @param actionType Type of the action to listen to.
    * @param callback Function to be called when the action is dispatched.
    */
-  declare function useHandler(actionType: string, callback: HandlerCallback): void;
+  export function useHandler(actionType: string, callback: HandlerCallback): void;
 
   /**
-   * Allows consuming a generator function, similar to how async actions work.
+   * Allows using a generator function to manipulate a state object asynchronously, similar to how async actions work.
    * @param generatorFunction Generator function to consume.
-   * @param defaultValue Default return value.
+   * @param initialState Initial state value.
    */
-  declare function useGenerator(generatorFunction: GeneratorFunction, defaultValue: any): any;
+  export function useAsync<T>(
+    generatorFunction: (...args) => Generator<Yieldable<T>, void, unknown>,
+    initialState?: T,
+  ): [T, ExecuteFunction<T>, CancelFunction];
 
   /**
    * Allows registering a reducer function that can listen to any action
@@ -130,7 +135,7 @@ declare module 'speedux' {
    * @param reducer Reducer function to use.
    * @param initialState Initial value for the state.
    */
-  declare function useReducer(
+  export function useReducer(
     key: string,
     reducer: ReducerFunction,
     initialState: GlobalStateObject,
@@ -140,12 +145,12 @@ declare module 'speedux' {
    * Allows using middleware functions such as React Router middleware and others.
    * @param middleware Middleware function to use.
    */
-  declare function useMiddleware(middleware: Middleware): void;
+  export function useMiddleware(middleware: Middleware): void;
 
-  declare function Provider(props: any): any;
+  export function Provider(props: any): any;
 
-  declare namespace store {
-    declare function create(): Store;
-    declare function getInstance(): Store;
+  export namespace store {
+    export function create(): Store;
+    export function getInstance(): Store;
   }
 }
